17043ce4a8d533490f7557273e79455fe791219e
小浩 <4213187@qq.com>
2021-01-14 17:26:43 +0800

    Initial commit

---
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -0,0 +1,17 @@
+package com.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lenovo
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
